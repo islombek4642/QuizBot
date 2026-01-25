e09ad1dfc178913326c556cba504b7891bd8d8f4
ihamidullayev <ihamidullayev5@gmail.com>
2026-01-25 23:02:23 +0500

    fix: update missing full_name and username for existing users

--- services/user_service.py
+++ services/user_service.py
@@ -20,10 +20,24 @@ class UserService:
             await self.db.refresh(user)
             is_new = True
             logger.info("New user created", telegram_id=telegram_id)
-        elif not user.is_active:
-            user.is_active = True
-            await self.db.commit()
-            logger.info("Inactive user became active", telegram_id=telegram_id)
+        else:
+            # Reactivate if inactive
+            needs_commit = False
+            if not user.is_active:
+                user.is_active = True
+                needs_commit = True
+                logger.info("Inactive user became active", telegram_id=telegram_id)
+            
+            # Update missing profile info from kwargs
+            if kwargs.get("full_name") and not user.full_name:
+                user.full_name = kwargs["full_name"]
+                needs_commit = True
+            if kwargs.get("username") and not user.username:
+                user.username = kwargs["username"]
+                needs_commit = True
+                
+            if needs_commit:
+                await self.db.commit()
         
         return user, is_new
 
